6bbcdfaa269622c5ff69d0820e891a37b3541f2d
gelsonw <gelsonw@gmail.com>
2017-12-08 21:16:10 -0200

    Adiciona teste para validação de jogada em posição existente

--- src/test/java/br/com/tasima/tictactoe/TicTacToeTests.java
+++ src/test/java/br/com/tasima/tictactoe/TicTacToeTests.java
@@ -5,21 +5,21 @@ import br.com.tasima.tictactoe.model.Partida;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TicTacToeTests {
 
     @Test
     public void criarPartida(){
-        Partida partida = new Partida("Partida 1");
+        Partida partida = populaPartida("Partida 1", "Jogador 0", "Jogador 1");
 
         assertEquals("Partida 1", partida.getId());
     }
 
     @Test
     public void adicionarJogadoresNaPartida(){
-        Partida partida = new Partida("Partida 2");
-        partida.setJogadorZero(new Jogador("Jogador 0", '0'));
-        partida.setJogadorUm(new Jogador("Jogador 1", '1'));
+        Partida partida = populaPartida("Partida 2", "Jogador 0", "Jogador 1");
 
         assertEquals("Jogador 0", partida.getJogadorZero().getNome());
         assertEquals("Jogador 1", partida.getJogadorUm().getNome());
@@ -27,21 +27,35 @@ public class TicTacToeTests {
 
     @Test
     public void exibirTabuleiroVazio(){
-        Partida partida = new Partida("Partida 3");
+        Partida partida = populaPartida("Partida 3", "Jogador 0", "Jogador 1");
 
         assertEquals("_|_|_\n_|_|_\n_|_|_", partida.getTabuleiro().toString());
     }
 
     @Test
-    public void realizarUmaJogada() {
-        Partida partida = new Partida("Partida 4");
-        partida.setJogadorZero(new Jogador("Jogador 0", '0'));
-        partida.setJogadorUm(new Jogador("Jogador 1", '1'));
+    public void realizarUmaJogadaComSucesso() {
+        Partida partida = populaPartida("Partida 4", "Jogador 0", "Jogador 1");
 
-        partida.executarJogada(partida.getJogadorZero(), 0, 0);
-
+        boolean resultadoJogada = partida.executarJogada(partida.getJogadorZero(), 0, 0);
+        assertTrue(resultadoJogada);
         assertEquals("0|_|_\n_|_|_\n_|_|_", partida.getTabuleiro().toString());
     }
 
+    @Test
+    public void realizarUmaJogadaComMarcacaoExistente() {
+        Partida partida = populaPartida("Partida 5", "Jogador 0", "Jogador 1");
 
+        boolean resultadoJogadaUm = partida.executarJogada(partida.getJogadorZero(), 0, 0);
+        boolean resultadoJogadaDois = partida.executarJogada(partida.getJogadorUm(), 0, 0);
+        assertTrue(resultadoJogadaUm);
+        assertFalse(resultadoJogadaDois);
+        assertEquals("0|_|_\n_|_|_\n_|_|_", partida.getTabuleiro().toString());
+    }
+
+    private Partida populaPartida(String idPartida, String nomeJogadorZero, String nomeJogadorUm) {
+        Partida partida = new Partida(idPartida);
+        partida.setJogadorZero(new Jogador(nomeJogadorZero, '0'));
+        partida.setJogadorUm(new Jogador(nomeJogadorUm, '1'));
+        return partida;
+    }
 }
